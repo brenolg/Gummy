04811d3f8ce015951d207751554a59014bffd671
brenolg <brenolgar@gmail.com>
2025-11-26 19:48:35 -0300

    how to use responsive

--- src/Pages/Home/HowToUse/index.tsx
+++ src/Pages/Home/HowToUse/index.tsx
@@ -1,18 +1,18 @@
-import {
-  Wrapper,
-  LeftBox,
-  Title,
-  List,
-  VideoBox,
-  BearImage,
-} from "./styles";
+import { Wrapper, LeftBox, Title, List, VideoBox, BearImage } from './styles'
 
-import bear from "./bear.svg";
-import { MainButton } from "@/components";
+import bear from '@/assets/imgs/bearHTU.svg'
+import { MainButton } from '@/components'
+import { useNavigate } from 'react-router-dom'
 
 export default function HowToUse() {
+  const navigate = useNavigate()
+
+  function goToCheckout() {
+    navigate('/checkout')
+  }
+
   return (
-    <Wrapper id='use'>
+    <Wrapper id="use">
       <LeftBox>
         <Title>Seu ritual diário</Title>
 
@@ -23,8 +23,9 @@ export default function HowToUse() {
           <li>Ideal para o life style moderno</li>
         </List>
 
-        <MainButton maxW={350}>Quero minha Power Gummy</MainButton>
-  
+        <MainButton maxW={350} onClick={goToCheckout}>
+          Quero minha Power Gummy
+        </MainButton>
         <BearImage src={bear} alt="gummy" />
       </LeftBox>
 
@@ -32,5 +33,5 @@ export default function HowToUse() {
         <video src="/videos/tutorial.mp4" controls />
       </VideoBox>
     </Wrapper>
-  );
+  )
 }
--- src/Pages/Home/HowToUse/styles.ts
+++ src/Pages/Home/HowToUse/styles.ts
@@ -1,38 +1,59 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   width: 100%;
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  gap: 40px;
   align-items: center;
-  justify-content: center;
-  position: relative;
+  justify-items: center;
   padding: 118px 123px 95px;
-  gap: 19px;
-  background: url(<path-to-image>) lightgray 0px -145.929px / 100% 154.964% no-repeat, var(--Roxo-0, #FDFBFE);
-`;
-
+  background:
+    url(<path-to-image>) lightgray 0px -145px / 100% 155% no-repeat,
+    var(--Roxo-0, #fdfbfe);
+  @media (max-width: 1100px) {
+    padding: 32px 24px;
+  }
+  @media (max-width: 900px) {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+`
 export const LeftBox = styled.div`
-  border-radius: 40px;
-  background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.20) 100%), var(--roxo-700, #502665);
-  display: flex;
-  width: 592px;
-  height: 318px;
-  padding: 42px 191px 33px 52px;
-  align-items: flex-start;
-  flex-shrink: 0;
   position: relative;
+  border-radius: 40px;
+  background:
+    linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.2) 100%), var(--roxo-700, #502665);
+  width: 100%;
+  max-width: 592px; /* limite máximo */
+  padding-left: 52px;
+  padding-top: 42px;
+  padding-bottom: 25px;
+  padding-right: 42px;
+  display: flex;
   flex-direction: column;
-`;
+  position: relative;
+  @media (max-width: 900px) {
+    justify-content: center;
+    align-items: center;
+    padding: 32px 16px;
+  }
+`
 
 export const Title = styled.h3`
-  color: var(--Roxo-0, #FDFBFE);
-  font-family: "Nunito Sans";
+  color: var(--Roxo-0, #fdfbfe);
+  font-family: 'Nunito Sans';
   font-size: 34px;
   font-style: normal;
   font-weight: 600;
   line-height: 120%; /* 40.8px */
-  margin-bottom:8px;
-`;
+  margin-bottom: 8px;
+  @media (max-width: 900px) {
+    font-size: 28px;
+    margin-bottom: 16px;
+  }
+`
 
 export const List = styled.ul`
   list-style: none;
@@ -40,47 +61,58 @@ export const List = styled.ul`
   margin: 0;
   margin-bottom: 8px;
   li {
-    color: var(--Roxo-0, #FDFBFE);
-    font-family: "Nunito Sans";
+    color: var(--Roxo-0, #fdfbfe);
+    font-family: 'Nunito Sans';
     font-size: 20px;
     font-style: normal;
     font-weight: 400;
     line-height: 150%; /* 30px */
-    position: relative; 
+    position: relative;
     padding-left: 20px;
     &::before {
-    content: "•";
+      content: '•';
       position: absolute;
       left: 0;
       top: 50%;
       transform: translateY(-50%);
-      color: var(--Roxo-0, #FDFBFE);
+      color: var(--Roxo-0, #fdfbfe);
       font-size: 20px;
     }
   }
-`;
-
+  @media (max-width: 900px) {
+    li {
+      font-size: 18px;
+    }
+  }
+`
 
 export const VideoBox = styled.div`
-  width: 592px;
+  width: 100%;
+  max-width: 592px;
   height: 318px;
+
   background: #f4f3f6;
   border-radius: 40px;
   overflow: hidden;
   position: relative;
-  z-index: 1;
 
   video {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
-`;
+`
 
 export const BearImage = styled.img`
   position: absolute;
-  left: 506px;
+  right: -75px;
   bottom: -29px;
   width: 150px;
   z-index: 3;
-`;
+  @media (max-width: 900px) {
+    right: -24px;
+    width: 90px;
+    height: 139px;
+    bottom: -21px;
+  }
+`
